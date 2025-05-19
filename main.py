655a2c7a12d89bd11cf9d80cd7b4d15b929ddc15
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-19 22:12:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from utils.extract import fetch_products
-from utils.transform import process_data
+from utils.transform import transform
 from utils.load import save_to_csv, save_to_google_sheets
 
 
 def main():
     raw_data = fetch_products(pages=50)
-    df = process_data(raw_data)
+    df = transform(raw_data)
     save_to_csv(df)
     save_to_google_sheets(df, json_keyfile='projectetl-460308-900545cba476.json')
 
