0bd25873a21f7d3e5c060b665831ede21b6dc67c
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-19 22:24:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from utils.load import save_to_csv, save_to_google_sheets
 def main():
     raw_data = fetch_products(pages=50)
     df = transform(raw_data)
+    print("✅ Data berhasil ditransformasi. Kolom:", df.columns.tolist())
+    print("🧾 Contoh data:\n", df.head())
     save_to_csv(df)
     save_to_google_sheets(df, json_keyfile='projectetl-460308-7f6b758c3790.json')
 
