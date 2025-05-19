b39f41214c2f4da30752b115dcc88046c1622315
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-19 22:16:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     raw_data = fetch_products(pages=50)
     df = transform(raw_data)
     save_to_csv(df)
-    save_to_google_sheets(df, json_keyfile='projectetl-460308-900545cba476.json')
+    save_to_google_sheets(df, json_keyfile='projectetl-460308-7f6b758c3790.json')
 
 if __name__ == "__main__":
     main()
