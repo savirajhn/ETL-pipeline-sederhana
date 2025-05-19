e6e3282449bc88364f6b9d78d4537dc8dd268817
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-19 18:13:59 +0700

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -5,7 +5,7 @@ from gspread_dataframe import set_with_dataframe
 def save_to_csv(df, filename='products.csv'):
     df.to_csv(filename, index=False)
 
-def save_to_google_sheets(df, json_keyfile='projectetl-460308-5f9f750dec66.json', sheet_url='https://docs.google.com/spreadsheets/d/1y4MjFD-gdprvLkIb7cEeH13x2aZAiWMiBzYVH5m2Md0/edit?usp=sharing'):
+def save_to_google_sheets(df, json_keyfile='projectetl-460308-c88ec62711c3.json', sheet_url='https://docs.google.com/spreadsheets/d/1y4MjFD-gdprvLkIb7cEeH13x2aZAiWMiBzYVH5m2Md0/edit?usp=sharing'):
     gc = gspread.service_account(filename=json_keyfile)
     sh = gc.open_by_url(sheet_url)
     worksheet = sh.sheet1
