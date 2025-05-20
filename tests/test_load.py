e3cd494701d552ea14d14438fc53fb9fc20356d4
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-20 13:38:10 +0700

    Update test_load.py

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,12 +1,12 @@
 import pytest
 import pandas as pd
 from unittest.mock import patch, MagicMock
-from utils.load import save_csv, save_to_google_sheets
+from utils import load as load
 
-def test_save_csv():
+def test_save_csv(tmp_path):
     df = pd.DataFrame({'a': [1, 2], 'b': [3, 4]})
-    filename = 'test_output.csv'
-    save_csv(df, filename)
+    filename = tmp_path / "test_output.csv"  # pakai direktori sementara
+    load.save_to_csv(df, filename)
     # Cek file dibuat dan isinya
     df_loaded = pd.read_csv(filename)
     assert list(df_loaded.columns) == ['a', 'b']
@@ -19,10 +19,9 @@ def test_save_to_google_sheets(mock_service_account):
     mock_worksheet = MagicMock()
     mock_service_account.return_value = mock_gc
     mock_gc.open_by_url.return_value = mock_sheet
-    mock_sheet.get_worksheet.return_value = mock_worksheet
+    mock_sheet.sheet1 = mock_worksheet
 
     df = pd.DataFrame({'a': [1], 'b': [2]})
-    save_to_google_sheets(df, 'dummy.json', 'https://dummyurl')
-    mock_gc.open_by_url.assert_called_once()
-    mock_sheet.get_worksheet.assert_called_once()
+    load.save_to_google_sheets(df, 'dummy.json', 'https://dummyurl')
+    mock_gc.open_by_url.assert_called_once_with('https://dummyurl')
     mock_worksheet.clear.assert_called_once()
