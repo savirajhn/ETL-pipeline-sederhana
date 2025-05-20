cc0ad6e251863da3bb765b8209b50d97d047bd1e
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-20 13:47:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     print("✅ Data berhasil ditransformasi. Kolom:", df.columns.tolist())
     print("🧾 Contoh data:\n", df.head())
     save_to_csv(df)
-    save_to_google_sheets(df, json_keyfile='projectetl-460308-7f6b758c3790.json')
+    save_to_google_sheets(df, json_keyfile='projectetl-460308-e98a6a1bcc21.json')
 
 if __name__ == "__main__":
     main()
